ba328798618560c02d104d138cb9d8c87f38dcd0
akua-star <akuafrimpomaa7461@gmail.com>
2025-08-13 20:47:10 -0400

    Add user-password.js and verify user.js models

--- models/user.js
+++ models/user.js
@@ -1,38 +1,9 @@
-const { Sequelize, DataTypes } = require('sequelize');
-
-module.exports = (sequelize) => {
-  const User = sequelize.define('user', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    encryption_key: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-  }, {
-    timestamps: false, // Explicitly disable timestamps
-    tableName: 'users', // Ensure correct table name
-    freezeTableName: true, // Prevent Sequelize from pluralizing table name
-  });
-
-  return User;
+export default (sequelize, DataTypes) => {
+  return sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    username: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    encryption_key: { type: DataTypes.STRING, allowNull: false },
+  }, { timestamps: false, tableName: 'users' });
 };
