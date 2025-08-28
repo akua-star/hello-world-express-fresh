dccf974d762057621a1f88f3741f777be4a5470d
akua-star <akuafrimpomaa7461@gmail.com>
2025-08-28 17:59:11 -0400

    use express-jwt auth

--- migrations/20250411204820-create-user-password.js
+++ migrations/20250411204820-create-user-password.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'User',
+          model: 'users',
           key: 'id'
         },
         onUpdate: 'CASCADE',
@@ -35,7 +35,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: true,
         references: {
-          model: 'User',
+          model: 'users',
           key: 'id'
         },
         onUpdate: 'CASCADE',
